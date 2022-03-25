c29ed8b9bb1f28e2a1a29996ae99d1629d3c48dc
Dima Portenko <troublesohard@gmail.com>
2022-03-25 12:40:51 +0200

    lesson-12: add base app with djangorestframework

---
+++ backend/base/urls.py
@@ -0,0 +1,8 @@
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('api/', views.getRoutes, name='routes'),
+    path('products/', views.getProducts, name='routes'),
+]
