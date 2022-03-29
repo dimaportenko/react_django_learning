4fb9a440a84ee88a1b6f1f7ccd14123fa7d2b17b
Dima Portenko <troublesohard@gmail.com>
2022-03-29 11:04:01 +0300

    lesson-15: add Review and Order modals

--- backend/base/models.py
+++ backend/base/models.py
@@ -19,3 +19,30 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+class Review(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=200, null=True, blank=True)
+    rating = models.IntegerField(null=True, blank=True, default=0)
+    comment = models.TextField(null=True, blank=True)
+    _id = models.AutoField(primary_key=True, editable=False)
+
+    def __str__(self):
+        return str(self.rating)
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    paymentMethod = models.CharField(max_length=200, null=True, blank=True)
+    taxPrice = models.DecimalField(max_digits=7, decimal_places=2)
+    shippingPrice = models.DecimalField(max_digits=7, decimal_places=2)
+    totalPrice = models.DecimalField(max_digits=7, decimal_places=2)
+    isPaid = models.BooleanField(default=False)
+    paidAt = models.DateTimeField(auto_now_add=False, blank=True, null=True)
+    isDelivered = models.DateTimeField(auto_now_add=False, blank=True, null=True)
+    deliveredAt = models.DateTimeField(auto_now_add=False)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    _id = models.AutoField(primary_key=True, editable=False)
+
+    def __str__(self):
+        return str(self.createdAt)
